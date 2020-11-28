e5249f5df9efe3ca6449477c513e5c85b88d6851
saftione <florian-hat@web.de>
2020-11-28 19:44:12 +0100

    enviroment

--- ngBee/src/app/_services/user.service.ts
+++ ngBee/src/app/_services/user.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
-const API_URL = 'http://localhost:8080/api/test/';
+
+
+const API_URL = `${environment.API_URL}/api/test/`;
+
+
 
 @Injectable({
   providedIn: 'root'

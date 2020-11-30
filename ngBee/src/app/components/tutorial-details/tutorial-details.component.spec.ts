012845bceac661f76cb15262adb07356d205b10e
saftione <florian-hat@web.de>
2020-11-30 17:54:27 +0100

    bnl

--- ngBee/src/app/components/tutorial-details/tutorial-details.component.spec.ts
+++ ngBee/src/app/components/tutorial-details/tutorial-details.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { TutorialDetailsComponent } from './tutorial-details.component';
 
@@ -6,7 +6,7 @@ describe('TutorialDetailsComponent', () => {
   let component: TutorialDetailsComponent;
   let fixture: ComponentFixture<TutorialDetailsComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ TutorialDetailsComponent ]
     })
--- ngBee/src/app/components/tutorials-list/tutorials-list.component.spec.ts
+++ ngBee/src/app/components/tutorials-list/tutorials-list.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { TutorialsListComponent } from './tutorials-list.component';
 
@@ -6,7 +6,7 @@ describe('TutorialsListComponent', () => {
   let component: TutorialsListComponent;
   let fixture: ComponentFixture<TutorialsListComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ TutorialsListComponent ]
     })
